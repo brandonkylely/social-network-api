0ae7a0ee1d8129e009475a77b3463de8a4815b5d
Brandon <brandonkly@ucla.edu>
2023-02-17 19:44:34 -0800

    added thought CRUD routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -4,27 +4,72 @@ const { Thought, Reaction} = require('../../models')
 
 //TODO: ROUTE TO GET ALL THOUGHTS
 router.get('/', (req,res)=> {
-
+    Thought.find({}, (err, result) => {
+        if (result) {
+          res.status(200).json(result);
+        } else {
+          console.log('Uh Oh, something went wrong');
+          res.status(500).json({ error: 'Something went wrong' });
+        }
+      });
 })
 
 //TODO: ROUTE TO CREATE A NEW THOUGHT
 router.post('/', (req,res)=> {
-
+    const newThought = new Thought({ thoughtText: req.body.thoughtText, username: req.body.username });
+    newThought.save();
+    if (newThought) {
+      res.status(200).json(newThought);
+    } else {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
 });
 
 //TODO: ROUTE TO GET SINGLE THOUGHT BASED ON THOUGHT ID
 router.get('/:thoughtId', (req,res)=> {
-
+    Thought.findOne({ _id: req.params.thoughtId }, (err, result) => {
+        if (result) {
+          res.status(200).json(result);
+        } else {
+          console.log('Uh Oh, something went wrong');
+          res.status(500).json({ error: 'Something went wrong' });
+        }
+      });
 })
 
 //TODO: ROUTE TO UPDATE A THOUGHT
-router.put('/', (req,res)=> {
-
+router.put('/:thoughtId', (req,res)=> {
+    Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { thoughtText: req.body.thoughtText },
+        { new: true },
+        (err, result) => {
+          if (result) {
+            res.status(200).json(result);
+            console.log(`Updated: ${result}`);
+          } else {
+            console.log('Uh Oh, something went wrong');
+            res.status(500).json({ message: 'something went wrong' });
+          }
+        }
+      );
 })
 
 //TODO: ROUTE TO DELETE A THOUGHT BASED ON THOUGHT ID
 router.delete('/:thoughtId', (req,res)=> {
-
+    Thought.findOneAndDelete(
+        { name: req.params.thoughtId },
+        (err, result) => {
+          if (result) {
+            res.status(200).json(result);
+            console.log(`Deleted: ${result}`);
+          } else {
+            console.log('Uh Oh, something went wrong');
+            res.status(500).json({ error: 'Something went wrong' });
+          }
+        }
+      );
 });
 
 //TODO: ROUTE TO ADD REACTION TO A THOUGHT
