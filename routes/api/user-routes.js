ca93baa86babe62e09f3dd98ae05d00ca835594b
Brandon <brandonkly@ucla.edu>
2023-02-17 22:11:00 -0800

    added user CRUD routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,27 +4,72 @@ const {User} = require("../../models")
 
 //TODO - ROUTE THAT GETS ALL THE USERS, include friends?
 router.get('/', (req,res)=> {
-
+    User.find({}, (err, result) => {
+        if (result) {
+          res.status(200).json(result);
+        } else {
+          console.log('Uh Oh, something went wrong');
+          res.status(500).json({ error: 'Something went wrong' });
+        }
+      });
 })
 
 //TODO - ROUTE THAT CREATES A NEW USER
 router.post('/', (req,res)=> {
-
+    const newUser = new User({ username: req.body.username, email: req.body.email });
+    newUser.save();
+    if (newUser) {
+      res.status(200).json(newUser);
+    } else {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ message: 'something went wrong' });
+    }
 });
 
 //TODO - ROUTE THAT GETS A SINGLE USER BASED ON USER ID
 router.get('/:userId', (req,res) => {
-
+    User.findOne({ _id: req.params.userId }, (err, result) => {
+        if (result) {
+          res.status(200).json(result);
+        } else {
+          console.log('Uh Oh, something went wrong');
+          res.status(500).json({ error: 'Something went wrong' });
+        }
+      });
 })
 
 //TODO - ROUTE THAT UPDATES A SINGLE USER
 router.put('/:userId', (req,res)=> {
-
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { username: req.body.username, email: req.body.email },
+        { new: true },
+        (err, result) => {
+          if (result) {
+            res.status(200).json(result);
+            console.log(`Updated: ${result}`);
+          } else {
+            console.log('Uh Oh, something went wrong');
+            res.status(500).json({ message: 'something went wrong' });
+          }
+        }
+      );
 })
 
 //TODO - ROUTE THAT DELETES A SINGLE USER BASED ON USER ID
 router.delete('/:userId', (req,res)=> {
-
+    User.findOneAndDelete(
+        { name: req.params.userId },
+        (err, result) => {
+          if (result) {
+            res.status(200).json(result);
+            console.log(`Deleted: ${result}`);
+          } else {
+            console.log('Uh Oh, something went wrong');
+            res.status(500).json({ error: 'Something went wrong' });
+          }
+        }
+      );
 });
 
 //TODO - ROUTE THAT ADDS A FRIEND TO A USER
